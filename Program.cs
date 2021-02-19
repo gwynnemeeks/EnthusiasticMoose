9a7aa12d5305594c510c813ebfc971edbe4ae4f4
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-02-19 13:46:51 -0600

    wip

--- Program.cs
+++ Program.cs
@@ -7,6 +7,40 @@ namespace EnthusiasticMoose
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+        }
+
+        static void MooseSays()
+        {
+            Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
         }
     }
 }
